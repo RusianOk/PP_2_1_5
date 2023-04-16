74a9ec4ad82382448e0481f146366f6d57835c5a
Ruslan Askhadullin <ashadullinruslan@rambler.ru>
2023-04-16 19:45:48 +0200

    first try

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,12 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component("deth8Bean")
 public class Deth8 {
+
+    @Override
+    public String toString() {
+        return "грустно";
+    }
 }
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,15 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("rabbit4Bean")
 public class Rabbit4 {
+    @Autowired
+    private Duck5 duck;
 
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck.toString();
     }
 }
